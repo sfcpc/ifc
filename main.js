0319126df9fe539c6feb773586d0fa0afdc6e15d
Rob Gaston <rgaston@fargeo.com>
2017-08-16 11:44:44 -0700

    moving livereload out of main.js

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 requirejs(['config'], function() {
-	if (window.location.origin === "file://") {
-		try {
-			require(['livereload']);
-		}  catch (e) {
-			console.log('cannot load livereload.')
-		}
-	}
     console.log('Hello World');
 });
