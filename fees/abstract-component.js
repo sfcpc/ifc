6b94f0edd65482907d221bc51aebe5686e9e0b1c
Rob Gaston <rgaston@fargeo.com>
2017-08-23 10:04:20 -0700

    adding comments to abstract fee modules

--- fees/abstract-component.js
+++ fees/abstract-component.js
@@ -2,7 +2,10 @@ define([
 	'knockout'
 ], function(ko) {
 	var AbstractComponent = function(params) {
+        // the fee viewmodel
 		this.fee = params.fee;
+
+        // a string indicating component state; 'form' or 'report'
 		this.state = params.state || 'form';
 	};
 	return AbstractComponent;
