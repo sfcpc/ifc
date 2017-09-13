1961669a888e014f722f225aba5fc3138a2c139a
Rob Gaston <rgaston@fargeo.com>
2017-09-13 10:31:05 -0700

    moving fee info to markdown file

--- fees/market-octavia-infrastructure/viewmodel.js
+++ fees/market-octavia-infrastructure/viewmodel.js
@@ -8,45 +8,6 @@ define([
 	'./component'
 ], function(ko, $, turf, AbstractFee, mapserverUtils, settings) {
 	var MarketOctaviaInfrastructureFee = function(params) {
-		// (a)   Application. Section 421.1et seq. shall apply to any development project located in the Market and Octavia Program Area as defined in Section 401.
-		// (b)   Projects subject to the Market and Octavia Community Improvement Impact Fee. The Market and Octavia Community Improvements Impact Fee is applicable to any development project in the Market and Octavia Program Area which results in:
-		//    (1)   At least one net new residential unit,
-		//    (2)   Additional space in an existing residential unit of more than 800 gross square feet,
-		//    (3)   At least one net new group housing facility or residential care facility,
-		//    (4)   Additional space in an existing group housing or residential care facility of more than 800 gross square feet,
-		//    (5)   New construction of a non-residential use, or
-		//    (6)   Additional non-residential space in excess of 800 gross square feet in an existing structure.
-		// (c)   Fee Calculation for the Market and Octavia Community Improvement Impact Fee. For development projects for which the Market and Octavia Community Improvements Impact Fee is applicable:
-		//    (1)   Any net addition of gross square feet shall pay per the Fee Schedule in Table 421.3A, and
-		//    (2)   Any replacement of gross square feet or change of use shall pay per the Fee Schedule in Table 421.3B.
-
-		// 421.3A
-		// FEE SCHEDULE FOR NET ADDITIONS OF GROSS SQUARE FEET IN THE MARKET AND OCTAVIA PROGRAM AREA
-		//
-		// Residential
-		// $9.00/gsf
-		//
-		// Non-residential
-		// $3.40/gsf
-		//
-		//
-		// 421.3B
-		// FEE SCHEDULE FOR REPLACEMENT OF USE OR CHANGE OF USE IN THE MARKET AND OCTAVIA PROGRAM AREA
-		// Residential to Residential or Non-residential; or Non-residential to Non-residential
-		//
-
-		// Residential to Residential or Non-residential; or Non-residential to Non-residential
-		// $0
-		//
-		// Non-Residential to Residential
-		// $5.60/gsf
-		//
-		// PDR to Residential
-		// $7.30/gsf
-		//
-		// PDR to Non-Residential
-		// $1.70/gsf
-
 		var self = this;
 		this.paramNames = [
 			'newRes',
