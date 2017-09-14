fc92565828dac4e5545c108e469356854d2b3388
Ryan Anderson <randerson@fargeo.com>
2017-09-14 12:11:11 -0700

    Updates to template and viewmodel

--- fees/eastern-neighborhoods-infrastructure/viewmodel.js
+++ fees/eastern-neighborhoods-infrastructure/viewmodel.js
@@ -26,9 +26,10 @@ define([
             return
                 this.newRes() !== null && this.newRes() !== '' &&
                 this.newNonRes() !== null && this.newNonRes() !== '' &&
+                this.newTIDF() !== null && this.newTIDF() !== '' &&
                 this.nonResToRes() !== null && this.nonResToRes() !== '' &&
                 this.pdrToRes() !== null && this.pdrToRes() !== '' &&
-                this.pdrToNonRes() !== null && this.pdrToNonRes() !== '';
+                this.pdrToNonRes() !== null && this.pdrToNonRes() !== '' &&
                 this.heightIncrease() !== null && this.heightIncrease() !== '';
         }, this);
 
@@ -38,7 +39,7 @@ define([
             var nonResToRes = this.nonResToRes() || 0;
             var pdrToRes = this.pdrToRes() || 0;
             var pdrToNonRes = this.pdrToNonRes() || 0;
-                        var heightIncrease = this.heightIncrease() || 0;
+            var heightIncrease = this.heightIncrease() || 0;
             if(!this.triggered()) {
                 return 0;
             }
@@ -50,7 +51,7 @@ define([
         }, this);
     };
 
-    EasternNeighborhoodsInfrastructureFee.feeTypeName = settings.name;
+    EasternNeighborhoodsInfrastructureFee.settings = settings;
 
     return EasternNeighborhoodsInfrastructureFee;
 });
