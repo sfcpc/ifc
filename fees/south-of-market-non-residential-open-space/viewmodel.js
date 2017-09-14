9f717493b33877b6105335b6c7298d2e9a6a2c3c
Jeffrey Munowitch <jmunowitch@fargeo.com>
2017-09-14 13:47:52 -0700

    Initial Commit

---
+++ fees/south-of-market-non-residential-open-space/viewmodel.js
@@ -0,0 +1,41 @@
+define([
+    'knockout',
+    'fees/abstract-fee',
+    'json!./settings.json',
+    './component'
+], function(ko, AbstractFee, settings) {
+    var somaNonResOpenSpace = function(params) {
+        AbstractFee.apply(this, [params]);
+
+        this.feeTypeName = settings.name;
+        this.label = settings.label;
+        this.value = ko.observable(params.value || null);
+        this.multiplier = settings.multiplier;
+
+        this.triggered = ko.computed(function() {
+            return this.app.netNewUnits() >= settings.minNetNewUnits;
+        }, this);
+
+        this.ready = ko.computed(function() {
+            if (!this.triggered()) {
+                return true;
+            }
+            return this.value();
+        }, this);
+
+        this.calculatedFee = ko.computed(function() {
+            var val = this.value();
+            return val && this.triggered() ? val * this.multiplier : 0;
+        }, this);
+
+        this.json = ko.computed(function() {
+            return {
+                "value": this.value()
+            };
+        }, this);
+    };
+
+    somaNonResOpenSpace.feeTypeName = settings.name;
+
+    return somaNonResOpenSpace;
+});
