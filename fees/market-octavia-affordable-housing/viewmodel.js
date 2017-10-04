be98998db9ae29eaf849774843be168868ea7ebc
Ryan Anderson <randerson@fargeo.com>
2017-10-04 13:04:58 -0700

    Add market and octavia affordable housing fee re #64

---
+++ fees/market-octavia-affordable-housing/viewmodel.js
@@ -0,0 +1,61 @@
+define([
+    'knockout',
+    'fees/abstract-fee',
+    'json!./settings.json',
+    './component'
+], function(ko, AbstractFee, settings) {
+    var MarketOctaviaAffordableHousingFee = function(params) {
+        this.settings = settings;
+
+        AbstractFee.apply(this, [params]);
+
+        this.district = ko.computed(function() {
+            var districts =  []
+            if (this.isProjectInArea()) {
+                this.isProjectInArea().forEach(function(e) {
+                    if (e.attributes.hasOwnProperty('districtname')) {
+                        districts.push(e.attributes.districtname)
+                    }
+                    else if (e.attributes.hasOwnProperty('name')) {
+                        districts.push(e.attributes.name)
+                    }
+                })
+            }
+            return districts
+        }, this)
+
+        this.triggered = ko.computed(function() {
+            return this.isProjectInArea() &&
+                (
+                    this.netNewUnits() >= this.minNetNewUnits
+                );
+        }, this);
+
+        this.ready = ko.computed(function() {
+            if (!this.triggered()) {
+                return true;
+            }
+            return this.newRes() !== null && this.newRes() !== '' &&
+                this.nonResToRes() !== null && this.nonResToRes() !== '' &&
+                this.pdrToRes() !== null && this.pdrToRes() !== '';
+        }, this);
+
+        this.calculatedFee = ko.computed(function() {
+            var newRes = this.newRes() || 0;
+            var nonResToRes = this.nonResToRes() || 0;
+            var pdrToRes = this.pdrToRes() || 0;
+            var district = this.district()[0];
+            if (!this.triggered()) {
+                return 0;
+            }
+
+            return (this[district].feePerNewRes * newRes) +
+                (this[district].feePerNonResToRes * nonResToRes) +
+                (this[district].feePerPDRToRes * pdrToRes);
+        }, this);
+    };
+
+    MarketOctaviaAffordableHousingFee.settings = settings;
+
+    return MarketOctaviaAffordableHousingFee;
+});
