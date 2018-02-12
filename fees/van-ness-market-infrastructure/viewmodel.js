f9089a96ce03ede549b4142e621f942e4c8cc395
Rob Gaston <rgaston@fargeo.com>
2018-02-12 13:58:55 -0800

    only calc fee for net new GSF re: #71

--- fees/van-ness-market-infrastructure/viewmodel.js
+++ fees/van-ness-market-infrastructure/viewmodel.js
@@ -12,7 +12,7 @@ define([
 
         this.triggered = this.isProjectInArea;
 
-        this.totalGSF = ko.computed(function() {
+        this.netNewGSF = ko.computed(function() {
             var total = parseFloat(this.nonResGSF()) +
                 parseFloat(this.pdrGSF()) +
                 parseFloat(this.resGSF()) +
@@ -39,23 +39,34 @@ define([
             return totalFAR > 9 ? totalGSF - (parcelArea * 9) : 0;
         }, this);
 
-        this.gsfAbove9To1Fee = ko.computed(function () {
-            var gsfAbove9To1 = this.gsfAbove9To1();
-            return gsfAbove9To1 * this.far9to1;
-        }, this);
-
         this.gsfAbove6To1 = ko.computed(function () {
             var totalFAR = this.totalFAR();
             var totalGSF = this.totalGSF();
             var parcelArea = this.parcelArea();
             var gsfAbove9To1 = this.gsfAbove9To1();
-            var val = totalFAR > 6 ? totalGSF - gsfAbove9To1 - (parcelArea * 6) : 0;
-            return val
+            return totalFAR > 6 ? totalGSF - gsfAbove9To1 - (parcelArea * 6) : 0;
+        }, this);
+
+        this.newGSFAbove6To1 = ko.computed(function () {
+            var gsfAbove6To1 = this.gsfAbove6To1();
+            var netNewGSF = this.netNewGSF();
+            return gsfAbove6To1 > netNewGSF ? netNewGSF : gsfAbove6To1;
         }, this);
 
         this.gsfAbove6To1Fee = ko.computed(function () {
-            var gsfAbove6To1 = this.gsfAbove6To1();
-            return gsfAbove6To1 * this.far6to1;
+            var newGSFAbove6To1 = this.newGSFAbove6To1();
+            return newGSFAbove6To1 * this.far6to1;
+        }, this);
+
+        this.newGSFAbove9To1 = ko.computed(function () {
+            var gsfAbove9To1 = this.gsfAbove9To1();
+            var remainingNewGSF = this.netNewGSF() - this.newGSFAbove6To1();
+            return gsfAbove9To1 > remainingNewGSF ? remainingNewGSF : gsfAbove9To1;
+        }, this);
+
+        this.gsfAbove9To1Fee = ko.computed(function () {
+            var newGSFAbove9To1 = this.newGSFAbove9To1();
+            return newGSFAbove9To1 * this.far9to1;
         }, this);
 
         this.calculatedFee = ko.computed(function() {
