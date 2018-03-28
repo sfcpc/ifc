2a180e0257d58840726507c0ff292fb16afb6a34
mikewynne <mike.wynne@sfgov.org>
2018-03-28 09:12:08 -0700

    #63 adding replacement

--- fees/eastern-neighborhoods-infrastructure/viewmodel.js
+++ fees/eastern-neighborhoods-infrastructure/viewmodel.js
@@ -27,9 +27,12 @@ define([
                 this.newNonRes() !== null && this.newNonRes() !== '' &&
                 this.nonResToRes() !== null && this.nonResToRes() !== '' &&
                 this.pdrToRes() !== null && this.pdrToRes() !== '' &&
-                this.pdrToNonRes() !== null && this.pdrToNonRes() !== '';
+                this.pdrToNonRes() !== null && this.pdrToNonRes() !== '' &&
+                this.nonResToResReplacement() !== null && this.nonResToResReplacement() !== '' &&
+                this.pdrToResReplacement() !== null && this.pdrToResReplacement() !== '' &&
+                this.pdrToNonResReplacement() !== null && this.pdrToNonResReplacement() !== '';
         }, this);
-        
+
         this.tier = ko.computed(function() {
             var areas = this.isProjectInArea();
             if (areas) {
@@ -53,7 +56,7 @@ define([
 
         this.calculatedFee = ko.computed(function() {
             if (!this.triggered()) {
-                return 0;    
+                return 0;
             }
             var tier = this.tier();
             var newRes = this.newRes() || 0;
@@ -61,12 +64,20 @@ define([
             var nonResToRes = this.nonResToRes() || 0;
             var pdrToRes = this.pdrToRes() || 0;
             var pdrToNonRes = this.pdrToNonRes() || 0;
+
+            var nonResToResReplacement = this.nonResToResReplacement() || 0;
+            var pdrToResReplacement = this.pdrToResReplacement() || 0;
+            var pdrToNonResReplacement = this.pdrToNonResReplacement() || 0;
             if (tier) {
+                //console.log(tier);
                 return (this.fees[tier].newRes * newRes) +
                     (this.fees[tier].newNonRes * newNonRes) +
-                    (this.fees[tier].nonResToRes * nonResToRes) +
-                    (this.fees[tier].pdrToRes * pdrToRes) +
-                    (this.fees[tier].pdrToNonRes * pdrToNonRes);
+                    (this.fees["Tier 1"].nonResToRes * nonResToRes) +
+                    (this.fees["Tier 1"].pdrToRes * pdrToRes) +
+                    (this.fees["Tier 1"].pdrToNonRes * pdrToNonRes) +
+                    (this.fees[tier].nonResToRes * nonResToResReplacement) +
+                    (this.fees[tier].pdrToRes * pdrToResReplacement) +
+                    (this.fees[tier].pdrToNonRes * pdrToNonResReplacement);
             }
             return null;
         }, this);
