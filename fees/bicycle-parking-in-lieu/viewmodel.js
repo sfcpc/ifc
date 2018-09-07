b1a63b26ecc7b62ff2e9b9fd4a3a2e3260681dcd
Jeffrey Munowitch <jmunowitch@fargeo.com>
2018-09-07 14:05:29 -0700

    Bicycle Parking In Lieu: Linting

--- fees/bicycle-parking-in-lieu/viewmodel.js
+++ fees/bicycle-parking-in-lieu/viewmodel.js
@@ -6,7 +6,7 @@ define([
     './component'
 ], function(ko, AbstractFee, settings, mapserverUtils) {
     var bicycleParkingInLeiu = function(params) {
-        var self = this;
+        // var self = this;
         this.settings = settings;
 
         AbstractFee.apply(this, [params]);
@@ -17,10 +17,10 @@ define([
 
         this.triggered = ko.computed(function() {
             return (
-                    this.netNewUnits() >= 1 ||
-                    this.resGFA() >= 1 ||
-                    this.netNewNonResGFA() >= 1
-                );
+                this.netNewUnits() >= 1 ||
+                this.resGFA() >= 1 ||
+                this.netNewNonResGFA() >= 1
+            );
         }, this);
 
         this.ready = ko.computed(function() {
@@ -30,20 +30,20 @@ define([
             return true;
         }, this);
 
-        this.maxSeniorDwellingUnits = ko.computed(function(){
+        this.maxSeniorDwellingUnits = ko.computed(function() {
             return this.netNewUnits() - this.studentDwellingUnits();
         }, this);
 
-        this.maxStudentDwellingUnits = ko.computed(function(){
+        this.maxStudentDwellingUnits = ko.computed(function() {
             return this.netNewUnits() - this.seniorDwellingUnits();
         }, this);
 
-        this.standardDwellingUnits = ko.computed(function(){
+        this.standardDwellingUnits = ko.computed(function() {
             return this.netNewUnits() - this.seniorDwellingUnits() - this.studentDwellingUnits();
         }, this);
 
         this.calculatedFee = ko.computed(function() {
-            if (!this.triggered() || this.numberOfBicycleSpacesInLeiu()== 0) {
+            if (!this.triggered() || this.numberOfBicycleSpacesInLeiu() === 0) {
                 return 0;
             }
             return this.numberOfBicycleSpacesInLeiu() * this.class2BicycleParkingInLeiuFee;
@@ -52,21 +52,20 @@ define([
         this.numberOfBicycleSpacesRequired = ko.computed(function() {
             return (
                 (this.studentDwellingUnits() / this.numberOfDwellingUnitsPerClass2BicycleParking * this.studentHousingMultiplier) +
-                ((this.seniorDwellingUnits()>=100) ? (this.seniorDwellingUnits() / this.numberOfSeniorHousingPerClass2BicycleParking * 2) : 2) +
-                (this.standardDwellingUnits()<=3 ? 0 : this.standardDwellingUnits() / this.numberOfDwellingUnitsPerClass2BicycleParking)
-            )
+                (this.seniorDwellingUnits() === 0) ? 0 : (((this.seniorDwellingUnits() >= 100) ? (this.seniorDwellingUnits() / this.numberOfSeniorHousingPerClass2BicycleParking * 2) : 2)) +
+                (this.standardDwellingUnits() <= 3 ? 0 : this.standardDwellingUnits() / this.numberOfDwellingUnitsPerClass2BicycleParking)
+            );
         }, this);
 
         this.maxInLeiuOption = ko.computed(function() {
             var max = (this.numberOfBicycleSpacesRequired() / 2);
-            if (max > this.maxInLeiu){
+            if (max > this.maxInLeiu) {
                 return this.maxInLeiu;
             } else if (max <= 1) {
                 return 0;
             }
             return max;
         }, this);
-
     };
 
     bicycleParkingInLeiu.settings = settings;
