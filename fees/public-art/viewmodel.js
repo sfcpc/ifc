dfba4be0e3b2ee17c3351daf620339ce62a7d5f8
Rob Gaston <rgaston@fargeo.com>
2018-09-17 11:38:35 -0700

    refactors public art fee, re: #78

--- fees/public-art/viewmodel.js
+++ fees/public-art/viewmodel.js
@@ -11,12 +11,8 @@ define([
 
         AbstractFee.apply(this, [params]);
 
-
         this.triggered = ko.computed(function() {
-            return this.isProjectInArea() &&
-                (
-                    this.firstConstructionDocument() === true
-                );
+            return this.isProjectInArea() && this.firstConstructionDocument();
         }, this);
 
         this.ready = ko.computed(function() {
@@ -24,34 +20,37 @@ define([
                 return true;
             }
 
-            if (this.nonResProject()) {
-                return this.constructionCost() !== null && this.constructionCost() !== '' && this.posRequirement() !== null && this.posRequirement() !== '';
-            }
-            else {
-                return this.constructionCost() !== null && this.constructionCost() !== ''
-            };
+            return this.constructionCost() !== null &&
+                this.constructionCost() !== '' &&
+                (
+                    !this.nonResProject() ||
+                    (
+                        this.posRequirement() !== null &&
+                        this.posRequirement() !== ''
+                    )
+                );
         }, this);
 
 
         this.calculatedFee = ko.computed(function() {
-            if (!this.triggered()) {
-                return 0;
+            var fee = 0;
+            var maxFee = this.maxNonResArtFeePOS1499;
+            if (this.triggered()) {
+                fee = this.constructionCost() * 0.01;
+
+                if (this.nonResProject()) {
+                    if (this.posRequirement() > 1499) {
+                        if (this.posRequirement() >= 3000){
+                            maxFee = this.maxNonResArtFeePOS3000;
+                        }
+                        if (fee > maxFee) {
+                            fee = maxFee;
+                        }
+                    }
+                }
             }
-                this.publicArtFee = ko.computed(function() {
-                    var fee = this.constructionCost() * 0.01
 
-                    if (this.nonResProject() && this.posRequirement() > 1499 && this.posRequirement() < 3000 && fee > this.maxNonResArtFeePOS1499) {
-                        fee = this.maxNonResArtFeePOS1499
-                    }
-                    else if (this.nonResProject() && this.posRequirement() >= 3000 && fee > this.maxNonResArtFeePOS3000) {
-                        fee = this.maxNonResArtFeePOS3000
-                    }
-
-                    return fee
-                }, this)
-
-
-                return this.publicArtFee()
+            return fee;
         }, this);
     };
 
