c28ac2a6b647d68ae2661549be3277f7fde1aa62
Rob Gaston <rgaston@fargeo.com>
2018-11-28 08:07:06 -0800

    updates for bike parking fee, re: #73

--- fees/bicycle-parking-in-lieu/viewmodel.js
+++ fees/bicycle-parking-in-lieu/viewmodel.js
@@ -2,17 +2,17 @@ define([
     'knockout',
     'fees/abstract-fee',
     'json!./settings.json',
-    'utils/mapserver',
     './component'
-], function(ko, AbstractFee, settings, mapserverUtils) {
-    var bicycleParkingInLieu = function(params) {
-        // var self = this;
+], function(ko, AbstractFee, settings) {
+    var BicycleParkingInLieu = function(params) {
         this.settings = settings;
 
         AbstractFee.apply(this, [params]);
 
         this.netNewNonResGFA = ko.computed(function() {
-            return parseFloat(this.nonResGFA()) + parseFloat(this.newNonRes());
+            var nonResGFA = parseFloat(this.nonResGFA());
+            var newNonRes = parseFloat(this.newNonRes());
+            return nonResGFA + newNonRes;
         }, this);
 
         this.triggered = ko.computed(function() {
@@ -27,75 +27,28 @@ define([
             if (!this.triggered()) {
                 return true;
             }
-            return this.numberOfBicycleSpacesInLieu() !== null && this.numberOfBicycleSpacesInLieu() !== '';
-        }, this);
-
-        this.maxSeniorDwellingUnits = ko.computed(function() {
-            return this.netNewUnits() - this.studentDwellingUnits();
-        }, this);
-
-        this.maxStudentDwellingUnits = ko.computed(function() {
-            return this.netNewUnits() - this.seniorDwellingUnits();
-        }, this);
-
-        this.standardDwellingUnits = ko.computed(function() {
-            return this.netNewUnits() - this.seniorDwellingUnits() - this.studentDwellingUnits();
+            var spacesInLieu = this.spacesInLieu();
+            return spacesInLieu !== null && spacesInLieu !== '';
         }, this);
 
         this.calculatedFee = ko.computed(function() {
-            if (!this.triggered() || this.numberOfBicycleSpacesInLieu() === 0) {
+            var spacesInLieu = this.spacesInLieu();
+            if (!this.triggered() || spacesInLieu === 0) {
                 return 0;
             }
-            return this.numberOfBicycleSpacesInLieu() * this.class2BicycleParkingInLieuFee;
+            return spacesInLieu * this.inLieuFee;
         }, this);
 
-        this.numberOfBicycleSpacesRequired = ko.computed(function() {
-            var residentialCalculation = this.netNewUnits() <= 3 ? 0
-                : ((this.studentDwellingUnits() / this.numberOfDwellingUnitsPerClass2BicycleParking) * this.studentHousingMultiplier) +
-                  ((this.seniorDwellingUnits() === '0' || this.seniorDwellingUnits() === null || this.seniorDwellingUnits() === '') ? 0
-                      : (this.seniorDwellingUnits() < 100 ? 2
-                          : (this.seniorDwellingUnits() / this.numberOfSeniorHousingPerClass2BicycleParking * 2))) +
-                  (this.standardDwellingUnits() / this.numberOfDwellingUnitsPerClass2BicycleParking);
-
-            var automotiveCalculation =
-                ((this.automotiveParkingSpots() === '0' || this.automotiveParkingSpots() === null || this.automotiveParkingSpots() === '') ? 0
-                    : Math.max(this.automotiveParkingSpots() / 20, 6)) +
-                    ((this.automotiveNonParking() === '0' || this.automotiveNonParking() === null || this.automotiveNonParking() === '') ? 0
-                        : (this.automotiveNonParking() > 5000 ? 4 : 2));
-
-            var entertainmentCalculation = (this.entertainmentArena() >= 2000 ? (this.entertainmentArena()) * 0.05 : 0) +
-                (this.entertainmentArts() > 0 ? Math.max(2, this.entertainmentArts() / 2500) : 0);
-            // this.entertainmentGeneral()
-
-            var industrialCalculation = this.industrialSpace() > 0 ? (this.industrialSpace() > 50000 ? 4 : 2) : 0;
-            var institutionalCalculation = (this.childCareChildren() / 20) +
-                (this.publicAccessibleGSF() / 2500 > 2 ? this.publicAccessibleGSF() / 2500 : 2);
-            var salesCalculation = 0;
-
-            return (
-                residentialCalculation +
-                automotiveCalculation +
-                entertainmentCalculation +
-                industrialCalculation +
-                institutionalCalculation +
-                salesCalculation
-            );
-        }, this);
-
-        this.maxInLieuOption = ko.computed(function() {
-            var max = (this.numberOfBicycleSpacesRequired() / 2);
-            if (this.numberOfBicycleSpacesRequired() <= 4) {
-                return this.numberOfBicycleSpacesRequired();
-            } else if (max > this.maxInLieu) {
-                return this.maxInLieu;
-            } else if (max <= 1) {
-                return 0;
-            }
-            return max;
-        }, this);
+        ko.computed(this.spacesInLieu)
+            .extend({ throttle: 600 })
+            .subscribe(function(spacesInLieu) {
+                if (parseFloat(spacesInLieu) > 20) {
+                    this.spacesInLieu(20);
+                }
+            }, this);
     };
 
-    bicycleParkingInLieu.settings = settings;
+    BicycleParkingInLieu.settings = settings;
 
-    return bicycleParkingInLieu;
+    return BicycleParkingInLieu;
 });
