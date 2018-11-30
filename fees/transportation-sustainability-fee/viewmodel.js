f485280d9ce638df4b2c7d9a1db31fb380bf0cae
Rob Gaston <rgaston@fargeo.com>
2018-11-30 11:41:19 -0800

    trigger logic and form for TSF, re: #58

--- fees/transportation-sustainability-fee/viewmodel.js
+++ fees/transportation-sustainability-fee/viewmodel.js
@@ -10,14 +10,36 @@ define([
         AbstractFee.apply(this, [params]);
 
         this.triggered = ko.computed(function() {
-            return false;
+            return this.netNewUnits() >= this.minNetNewUnits ||
+                this.resGFA() >= this.minResGFA ||
+                this.nonResGFA() >= this.minNonResGFA ||
+                this.pdrGFA() >= this.minPDRGFA ||
+                this.changeOfUse() > this.minChangeOfUse;
         }, this);
 
         this.ready = ko.computed(function() {
             if (!this.triggered()) {
                 return true;
             }
-            return false;
+            return this.hospitalGFA() !== null && this.hospitalGFA() !== '' &&
+                this.healthGFA() !== null && this.healthGFA() !== '' &&
+                this.resToNonRes() !== null && this.resToNonRes() !== '' &&
+                this.resToHealth() !== null && this.resToHealth() !== '' &&
+                this.resToHospital() !== null && this.resToHospital() !== '' &&
+                this.hospitalToRes() !== null && this.hospitalToRes() !== '' &&
+                this.hospitalToNonRes() !== null && this.hospitalToNonRes() !== '' &&
+                this.hospitalToPDR() !== null && this.hospitalToPDR() !== '' &&
+                this.hospitalToHealth() !== null && this.hospitalToHealth() !== '' &&
+                this.healthToRes() !== null && this.healthToRes() !== '' &&
+                this.healthToNonRes() !== null && this.healthToNonRes() !== '' &&
+                this.healthToPDR() !== null && this.healthToPDR() !== '' &&
+                this.healthToHospital() !== null && this.healthToHospital() !== '' &&
+                this.pdrToRes() !== null && this.pdrToRes() !== '' &&
+                this.pdrToNonRes() !== null && this.pdrToNonRes() !== '' &&
+                this.pdrToHealth() !== null && this.pdrToHealth() !== '' &&
+                this.pdrToHospital() !== null && this.pdrToHospital() !== '' &&
+                this.newNewHospitalBeds() !== null && this.newNewHospitalBeds() !== '' &&
+                this.totalHospitalOperatorBeds() !== null && this.totalHospitalOperatorBeds() !== '';
         }, this);
 
         this.calculatedFee = ko.computed(function() {
