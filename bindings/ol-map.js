8381f8b1e4f35b548be7b5baa47dc35097f53032
Rob Gaston <rgaston@fargeo.com>
2018-12-03 15:16:53 -0800

    fixes for public art fee

--- bindings/ol-map.js
+++ bindings/ol-map.js
@@ -10,7 +10,7 @@ define([
             var readOnly = allBindingsAccessor().readOnly;
             var selectedPoint = allBindingsAccessor().selectedPoint || ko.observable();
             var onMapReady = allBindingsAccessor().onMapReady || function() {
-                return
+                return;
             };
             var esrijsonFormat = new ol.format.EsriJSON();
             var geojsonFormat = new ol.format.GeoJSON();
@@ -38,11 +38,7 @@ define([
                         url: settings.mapserver,
                         params: {
                             layers: 'show:' + [
-                                settings.somaDistrictsLayer,
-                                settings.downtownZoningLayer,
-                                settings.vanNessSUDLayer,
-                                settings.upperMarketNCTLayer,
-                                settings.areaLayer
+                                settings.downtownZoningLayer
                             ].join(',')
                         }
                     }),
@@ -88,7 +84,7 @@ define([
 
             if (geometry()) {
                 var geom = geojsonFormat.readGeometry(geometry());
-                view.fit(geom, map.getSize())
+                view.fit(geom, map.getSize());
             }
 
             selectedPoint.subscribe(function(point) {
@@ -146,7 +142,7 @@ define([
                         }
                     });
                 }
-            })
+            });
 
             if (!readOnly) {
                 map.on('click', function(e) {
