6a89ad4f9a23a57c605b0d0c834a6b54d5e59656
sherm17 <31725260+sherm17@users.noreply.github.com>
2019-07-29 15:35:06 -0700

    add period in index.html
    add period in index.html

--- fees/north-of-market-residential-special-use-district-affordable-housing-fee/viewmodel.js
+++ fees/north-of-market-residential-special-use-district-affordable-housing-fee/viewmodel.js
@@ -24,7 +24,7 @@ define([
                 return 0;
             }
             return this.resGFA() * this.costPerSquareFoot;
-        }, this);
+        }, this); 
     };
 
     NorthOfMarketFee.settings = settings;
