d2417c13571229495441da281979f3684b50f9bf
njkim <nkim@fargeo.com>
2020-01-15 11:29:37 -0800

    Update Job-Housing Linkage fees fro 2020 (Office and Lab)

--- fees/jobs-housing-linkage/viewmodel.js
+++ fees/jobs-housing-linkage/viewmodel.js
@@ -62,8 +62,8 @@ define([
             var feePerNewEntertainment = oldPDR ? this.oldFeePerNewEntertainment : this.feePerNewEntertainment;
             var feePerNewHotel = oldPDR ? this.oldFeePerNewHotel : this.feePerNewHotel;
             var feePerNewIntegratedPDR = oldPDR ? this.oldFeePerNewIntegratedPDR : this.feePerNewIntegratedPDR;
-            var feePerNewOffice = oldPDR ? this.oldFeePerNewOffice : this.officeNewRates();
-            var feePerNewResearchAndDevelopment = oldPDR ? this.oldFeePerNewResearchAndDevelopment : this.feePerNewResearchAndDevelopment;
+            var feePerNewOffice = oldPDR ? (this.oldFeePerNewOffice-20.71) : this.officeNewRates();
+            var feePerNewResearchAndDevelopment = oldPDR ? this.oldFeePerNewResearchAndDevelopment : this.labNewRates();
             var feePerNewRetail = oldPDR ? this.oldFeePerNewRetail : this.feePerNewRetail;
             var feePerNewSmallEnterpriseWorkspace = oldPDR ? this.oldFeePerNewSmallEnterpriseWorkspace : this.feePerNewSmallEnterpriseWorkspace;
             ent = ent() || 0;
