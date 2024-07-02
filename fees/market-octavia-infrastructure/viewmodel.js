c263510b0864b5b6f920b6c8406fb8979e6cab01
Jason Sherba <JSHERBA@cityplanning.sfgov.org>
2024-07-02 08:40:18 -0700

    fee edits sherba 7/2024

--- fees/market-octavia-infrastructure/viewmodel.js
+++ fees/market-octavia-infrastructure/viewmodel.js
@@ -30,9 +30,25 @@ define([
                 this.pdrToNonRes() !== null && this.pdrToNonRes() !== '';
         }, this);
 
+        // calculate portion resNew by removing nonResToRes and pdrToRes from resNew
+        this.resNewPortion = ko.computed(function() {
+            var newPortion = parseFloat(this.newRes()) - (
+                parseFloat(this.nonResToRes()) +
+                parseFloat(this.pdrToRes())
+            );
+            return newPortion > 0 ? newPortion : 0;
+        }, this);
+
+        this.nonResNewPortion = ko.computed(function() {
+            var newPortion = parseFloat(this.newNonRes()) - (
+                parseFloat(this.pdrToNonRes())
+            );
+            return newPortion > 0 ? newPortion : 0;
+        }, this);
+
         this.calculatedFee = ko.computed(function() {
-            var newRes = this.newRes() || 0;
-            var newNonRes = this.newNonRes() || 0;
+            var newRes = this.resNewPortion() || 0;
+            var newNonRes = this.nonResNewPortion() || 0;
             var nonResToRes = this.nonResToRes() || 0;
             var pdrToRes = this.pdrToRes() || 0;
             var pdrToNonRes = this.pdrToNonRes() || 0;
